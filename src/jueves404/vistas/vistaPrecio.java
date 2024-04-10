3c5a8a45c03ebb8bd729be9fe600280212fcf1f6
RamiroMRomero <ramiromateoromero@sanluis.edu.ar>
2024-04-10 12:12:55 -0300

    vistaPrecio
    Cambio de nombre de variable para las tablas.

--- src/jueves404/vistas/vistaPrecio.java
+++ src/jueves404/vistas/vistaPrecio.java
@@ -32,7 +32,7 @@ public class vistaPrecio extends javax.swing.JInternalFrame {
         jtPrecioMaximo = new java.awt.TextField();
         jLabel3 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        jtblProductos = new javax.swing.JTable();
 
         jLabel1.setText("BUSCAR POR PRECIO");
 
@@ -49,7 +49,7 @@ public class vistaPrecio extends javax.swing.JInternalFrame {
 
         jLabel3.setText("y");
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        jtblProductos.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
                 {null, null, null, null},
@@ -60,7 +60,7 @@ public class vistaPrecio extends javax.swing.JInternalFrame {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
-        jScrollPane1.setViewportView(jTable1);
+        jScrollPane1.setViewportView(jtblProductos);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -113,8 +113,8 @@ public class vistaPrecio extends javax.swing.JInternalFrame {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
     private java.awt.TextField jtPrecioMaximo;
     private java.awt.TextField jtPrecioMinimo;
+    private javax.swing.JTable jtblProductos;
     // End of variables declaration//GEN-END:variables
 }
